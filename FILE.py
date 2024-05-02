e4b73797a45669a0b127b225269a631c7e6f32e9
ARAFAT AHAMMED <142329075+SPEED-ARAFAT@users.noreply.github.com>
2024-05-02 15:34:22 +0600

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -1,3 +1,13 @@
-import os
-import sys
-os.system('chmod 777 FILE64;./FILE64')
+import os, sys, platform,time
+ 
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('clear')
+    os.system('git pull')
+    os.system('chmod 777 FILE64')
+    import FILE64    
+elif bit == '32bit':
+    os.system('clear')
+    os.system('git pull')
+    os.system('chmod 777 FILE32')
+    import FILE32
